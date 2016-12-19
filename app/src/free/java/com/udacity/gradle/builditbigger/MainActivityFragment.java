9747ffbc376dea0386f0ef0e201a39fc52be8549
Dinesh Prasanth M K <mkdineshprasanth@gmail.com>
2016-12-18 21:10:03 -0500

    Removed Key!

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -34,7 +34,6 @@ public class MainActivityFragment extends BasicJokesProcessor {
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice("B9760DAC7975BAFFEDAE8B3C248F0C20")
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
 
@@ -62,7 +61,6 @@ public class MainActivityFragment extends BasicJokesProcessor {
 
     private void requestNewInterstitial() {
         AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice("B9760DAC7975BAFFEDAE8B3C248F0C20")
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mInterstitialAd.loadAd(adRequest);
